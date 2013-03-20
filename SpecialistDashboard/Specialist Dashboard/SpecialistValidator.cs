07af6f6d811a749d000d0c2948d656fb90132ad4
Reed Wilson <reedcwilson@gmail.com>
2013-03-20 07:25:12 -0600

    Make an Editable Specialist Validator
    Make a files for auditors and supervisors that can be read and used to
    validate.

--- SpecialistDashboard/Specialist Dashboard/SpecialistValidator.cs
+++ SpecialistDashboard/Specialist Dashboard/SpecialistValidator.cs
@@ -2,37 +2,52 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 
 namespace Specialist_Dashboard
 {
     class SpecialistValidator
     {
-        public static string[] QEAuditors = new string[]
+        private static List<string> ValidAuditors;
+        public static List<string> GetValidAuditors()
         {
-            "miranda.milligan", 
-            "samuel.butler", 
-            "reed.wilson", 
-            "david.crockett",
-            "andrew.thomas",
-            "scott.spencer",
-            "christpher.felt",
-            "mike.gillotti",
-            "tamra.ockey",
-            "ryan.smith"
-        };
+            ValidAuditors = ReadValids(@"\\dpfs01\dpsfiler\Imaging\Production_Team\Supervisors\SpecDashValidator\ValidAuditors.txt");
+            return ValidAuditors;
+        }
 
-        public static string[] Supervisors = new string[]
+        private static List<string> ValidSupers;
+        public static List<string> GetValidSupers()
         {
-            "miranda.milligan", 
-            "samuel.butler", 
-            "reed.wilson", 
-            "david.crockett"
-        };
+            ValidSupers = ReadValids(@"\\dpfs01\dpsfiler\Imaging\Production_Team\Supervisors\SpecDashValidator\ValidSupers.txt");
+            return ValidSupers;
+        }
+
+        //public static string[] QEAuditors = new string[]
+        //{
+        //    "miranda.milligan", 
+        //    "samuel.butler", 
+        //    "reed.wilson", 
+        //    "david.crockett",
+        //    "andrew.thomas",
+        //    "scott.spencer",
+        //    "christpher.felt",
+        //    "mike.gillotti",
+        //    "tamra.ockey",
+        //    "ryan.smith"
+        //};
+
+        //public static string[] Supervisors = new string[]
+        //{
+        //    "miranda.milligan", 
+        //    "samuel.butler", 
+        //    "reed.wilson", 
+        //    "david.crockett"
+        //};
 
         public static bool ValidateAuditor()
         {
             string userName = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
-            foreach (var auditor in QEAuditors)
+            foreach (var auditor in GetValidAuditors())
             {
                 if (@"myfamily\" + auditor == userName.ToLower())
                     return true;
@@ -43,12 +58,28 @@ namespace Specialist_Dashboard
         public static bool ValidateSupervisor()
         {
             string userName = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
-            foreach (var specialist in Supervisors)
+            foreach (var specialist in GetValidSupers())
             {
                 if (@"myfamily\" + specialist == userName.ToLower())
                     return true;
             }
             return false;
         }
+
+        private static List<string> ReadValids(string path)
+        {
+            var valids = new List<string>();
+            string line;
+
+            using (var sr = new StreamReader(path))
+            {
+                while ((line = sr.ReadLine()) != null)
+                    if (line.Trim() != "")
+                        valids.Add(line.Trim());
+            }
+            return valids;
+        }
+
+
     }
 }
